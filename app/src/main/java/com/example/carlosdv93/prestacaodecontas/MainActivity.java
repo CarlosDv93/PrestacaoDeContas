6c519281dff38a306c5b0a55c6dbd01bb3705df4
CarlosDv93 <carlos9_cv@hotmail.com>
2016-07-30 10:05:12 -0300

    Somente numeros com valores decimais no campo valor.

--- app/src/main/java/com/example/carlosdv93/prestacaodecontas/MainActivity.java
+++ app/src/main/java/com/example/carlosdv93/prestacaodecontas/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -50,6 +51,7 @@ public class MainActivity extends MenuAll{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        //etxtValor.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
     }
 
     public void criaAlerta(char tipo){
